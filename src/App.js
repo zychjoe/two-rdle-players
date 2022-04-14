9623ba22217194e0987eb2f0326add2c389afd37
Joe Zedd <zychjoe@gmail.com>
2022-04-14 17:18:04 -0400

    letter functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import GuessLetter from './GuessLetter.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GuessLetter value="A" result="" />
+      <GuessLetter value="B" result="close" />
+      <GuessLetter value="C" result="perfect" />
+      <GuessLetter value="D" result="" />
+      <GuessLetter value="E" result="" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
