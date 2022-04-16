620fb4671e0490c48869de92e5ea518a675ea0ba
Joe Zedd <zychjoe@gmail.com>
2022-04-16 16:35:28 -0400

    cleaned and commented OSKey.js

--- src/OSKey.js
+++ src/OSKey.js
@@ -1,6 +1,19 @@
 import React from "react"
 import './OSKey.css'
 
+/*
+ * OSKey (short for On-Screen Key) makes up our On-Screen Keyboard (OSKB).
+ * 
+ * The 'type' will be either "leter" or "function". There will be twenty-six
+ * (26) "letter" OSKeys, one for each letter in the alphabet. Their
+ * arrangement will macth a QWERTY keyboard.
+ * 
+ * There will be two (2) "action" keys in the OSKB; 'ENTER' and 'DEL'. Their
+ * functions will vary from screen to screen.
+ * 
+ * Any other screens which need player input through buttons will utilize
+ * an 'action' OSKey. 
+ */
 function OSKey(props){
     return(
         <button className="osk" 
