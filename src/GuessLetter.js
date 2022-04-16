77fb03a3bc01ff27f31cef0d3ea0e1ab0d3224be
Joe Zedd <zychjoe@gmail.com>
2022-04-16 16:22:39 -0400

    cleaned and commented GuessLetter.js

--- src/GuessLetter.js
+++ src/GuessLetter.js
@@ -11,11 +11,10 @@ import './GuessLetter.css'
  * screen keyboard (OSK). It will have a letter only after being input by the
  * OSK.
  * 
- * The 'result' value is the empty string by default. It is also the empty
- * string if, after the guess is evaluated, the letter is not contained in the
- * answer. If the letter exists in the answer but in a different position the
- * 'result' will be "close". If the letter is correctly positioned, the
- * 'result' will be "perfect".
+ * The 'result' value is the empty string by default.bIf the letter guessed
+ * exists in the answer but in a different position the 'result' will be
+ * "close". If the letter is correctly positioned, the 'result' will be
+ * "perfect". If the letter is not in the answer at all, 'result' is "miss".
  */
 function GuessLetter(props){
 
